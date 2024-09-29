29e910c9ad157bb8f2da1f495fa7e21b1b4966fe
Aravind <aravindmunna240@gmail.com>
2024-09-29 15:36:30 +0530

    initial setups defining controllers, ui thymeleaf and services

--- src/main/java/com/buildlive/guesstheword/service/GameService.java
+++ src/main/java/com/buildlive/guesstheword/service/GameService.java
@@ -1,7 +1,10 @@
 package com.buildlive.guesstheword.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Random;
 
+@Service
 public class GameService {
 
     private String[] randomWords = {"father","mother","sister,string","hello","light"};
