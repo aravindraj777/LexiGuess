f836f5ed651e93b6e966e9573b9faba6a0423ea2
Aravind <aravindmunna240@gmail.com>
2024-09-29 15:35:35 +0530

    Defining thymeleaf models and send to ui through controller methods

--- src/main/java/com/buildlive/guesstheword/controller/GameController.java
+++ src/main/java/com/buildlive/guesstheword/controller/GameController.java
@@ -1,2 +1,27 @@
-package com.buildlive.guesstheword.controller;public class GameController {
+package com.buildlive.guesstheword.controller;
+
+import com.buildlive.guesstheword.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/game")
+public class GameController {
+
+
+
+    @Autowired
+    GameService gameService;
+
+    @GetMapping("/home")
+    public String showHomePage(Model model){
+       String randomWord =  gameService.toString();
+       model.addAttribute("randomWord",randomWord);
+       return "game-home-page";
+    }
+
+
 }
